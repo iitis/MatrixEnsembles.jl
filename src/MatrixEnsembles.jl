b70976cc5f45408ff9d213b156a229cb013b8746
Łukasz Pawela <lukasz.pawela@gmail.com>
2020-10-30 00:36:52 +0100

    move files to new repo

--- src/MatrixEnsembles.jl
+++ src/MatrixEnsembles.jl
@@ -13,4 +13,23 @@ include("ginibre.jl")
 include("circular.jl")
 include("wigner.jl")
 include("wishart.jl")
+
+function __init__()
+    @require CUDA="052768ef-5323-5732-b1bb-66c8b64840ba" begin
+        if CUDA.functional() && CUDA.has_cutensor()
+            const CuArray = CUDA.CuArray
+            const CuVector = CUDA.CuVector
+            const CuMatrix = CUDA.CuMatrix
+            const CuSVD = CUDA.CUSOLVER.CuSVD
+            const CuQR = CUDA.CUSOLVER.CuQR
+            # scalar indexing is fine before 0.2
+            # CUDA.allowscalar(false)
+            export curand
+            include("cuda/circular.jl") 
+            include("cuda/ginibre.jl")
+            include("cuda/wigner.jl")
+            include("cuda/wishart.jl")
+        end
+    end
+end
 end
